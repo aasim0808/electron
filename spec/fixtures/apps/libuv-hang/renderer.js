745ca856ce868f155c7363ebc40120d03daa42cd
aasim0808 <aakimkhan690@gmail.com>
2023-10-21 05:37:20 +0530

    Delete spec directory

--- spec/fixtures/apps/libuv-hang/renderer.js
+++
@@ -1,6 +0,0 @@
-const { run, ipcRenderer } = window.api;
-
-run().then(async () => {
-  const count = await ipcRenderer.invoke('reload-successful');
-  if (count < 3) location.reload();
-}).catch(console.log);
